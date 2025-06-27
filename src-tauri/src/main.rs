074669f1bbfeff68b5dd0325e81c1cd1dc4856cf
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2025-06-28 03:49:35 +0900

    feat: log機能追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::{fs::File};
 
+use gag::Redirect;
 fn main() {
+    let fs = File::create("./logfile.log").unwrap();
+    let _redirect = Redirect::stdout(fs).expect("Failed to redirect stdout");
+    let fs_stderr = File::create("./stderr.log").unwrap();
+    let _redirect = Redirect::stderr(fs_stderr).expect("Failed to redirect stderr");
     tuari_template_lib::run()
 }
